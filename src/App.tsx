7e0ad04b5d0726f8dd555031cbcf4809b45d75d0
Warrior <react.js.warrior@gmail.com>
2021-08-20 18:31:44 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const context = canvas.getContext("2d");
+    contextRef.current = context;
+  }, []);
+
+  const startDraw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { x, y } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(x, y);
+    setIsDrawing(true);
+  };
+
+  const finishDraw = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { x, y } = nativeEvent;
+    contextRef.current.lineTo(x, y);
+    contextRef.current.stroke();
+  };
+
+  const clear = () => {
+    contextRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+  };
+
+  const save = () => {
+    setImages((prev) => [...prev, canvasRef.current.toDataURL()]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={clear}>clear</button>
+      <button onClick={save}>save</button>
+      <canvas
+        id="canvas"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseUp={finishDraw}
+        onMouseDown={startDraw}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
+      {images.length
+        ? images.map((item, index) => (
+            <img key={index} src={`${item}`} alt="img" />
+          ))
+        : null}
+    </>
   );
 }
 
