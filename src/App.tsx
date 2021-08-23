581a411da51a5be270e81988d6b3ccb8027cddc7
Warrior <react.js.warrior@gmail.com>
2021-08-23 12:40:27 +0300

    config eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,82 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useRef } from "react";
-import "./App.css";
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import './App.css'
 
-function App() {
-  const canvasRef = useRef(null);
-  const contextRef = useRef(null);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [images, setImages] = useState([]);
+function App(): JSX.Element {
+    const canvasRef = useRef(null)
+    const contextRef = useRef(null)
+    const [isDrawing, setIsDrawing] = useState(false)
+    const [images, setImages] = useState([])
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    useEffect(() => {
+        const canvas = canvasRef.current
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
 
-    const context = canvas.getContext("2d");
-    contextRef.current = context;
-  }, []);
+        const context = canvas.getContext('2d')
+        contextRef.current = context
+    }, [])
 
-  const startDraw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
-    const { x, y } = nativeEvent;
-    contextRef.current.beginPath();
-    contextRef.current.moveTo(x, y);
-    setIsDrawing(true);
-  };
-
-  const finishDraw = () => {
-    contextRef.current.closePath();
-    setIsDrawing(false);
-  };
-
-  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!isDrawing) {
-      return;
+    const startDraw = ({
+        nativeEvent,
+    }: React.MouseEvent<HTMLCanvasElement>): void => {
+        const { x, y } = nativeEvent
+        contextRef.current.beginPath()
+        contextRef.current.moveTo(x, y)
+        setIsDrawing(true)
     }
-    const { x, y } = nativeEvent;
-    contextRef.current.lineTo(x, y);
-    contextRef.current.stroke();
-  };
 
-  const clear = () => {
-    contextRef.current.clearRect(
-      0,
-      0,
-      canvasRef.current.width,
-      canvasRef.current.height
-    );
-  };
+    const finishDraw = (): void => {
+        contextRef.current.closePath()
+        setIsDrawing(false)
+    }
 
-  const save = () => {
-    setImages((prev) => [...prev, canvasRef.current.toDataURL()]);
-  };
+    const draw = ({
+        nativeEvent,
+    }: React.MouseEvent<HTMLCanvasElement>): void => {
+        if (!isDrawing) {
+            return
+        }
+        const { x, y } = nativeEvent
+        contextRef.current.lineTo(x, y)
+        contextRef.current.stroke()
+    }
 
-  return (
-    <>
-      <button onClick={clear}>clear</button>
-      <button onClick={save}>save</button>
-      <canvas
-        id="canvas"
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onMouseUp={finishDraw}
-        onMouseDown={startDraw}
-        onMouseMove={draw}
-        ref={canvasRef}
-      />
-      {images.length
-        ? images.map((item, index) => (
-            <img key={index} src={`${item}`} alt="img" />
-          ))
-        : null}
-    </>
-  );
+    const clear = (): void => {
+        contextRef.current.clearRect(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        )
+    }
+
+    const save = (): void => {
+        setImages((prev) => [...prev, canvasRef.current.toDataURL()])
+    }
+
+    return (
+        <>
+            <button onClick={clear}>clear</button>
+            <button onClick={save}>save</button>
+            <canvas
+                id="canvas"
+                width={window.innerWidth}
+                height={window.innerHeight}
+                onMouseUp={finishDraw}
+                onMouseDown={startDraw}
+                onMouseMove={draw}
+                ref={canvasRef}
+            />
+            {images.length
+                ? images.map((item, index) => (
+                      <img key={index} src={`${item}`} alt="img" />
+                  ))
+                : null}
+        </>
+    )
 }
 
-export default App;
+export default App
